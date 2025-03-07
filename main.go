f5d22bd73b68d217515af25440f639f1f5ae35f2
Kevin Huang <kevinht.huang@gmail.com>
2025-03-07 00:47:41 -0800

    working prototype, need to add follow-up gif or image

--- main.go
+++ main.go
@@ -4,131 +4,81 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
+	"net/url"
 	"os"
 	"os/signal"
+	"path/filepath"
+	"slices"
 
 	"github.com/bwmarrin/discordgo"
 	"gopkg.in/yaml.v3"
 )
 
 // config for the overall application
-type secretsConfig struct {
+type discordConfig struct {
 	GuildID  string `yaml:"guildId"`
 	BotToken string `yaml:"botToken"`
 }
 
 type heroConfig struct {
-	Heroes *[]heroData `yaml:heroes`
+	Heroes *[]hero `yaml:heroes`
 }
 
 // config for each hero
-type heroData struct {
+type hero struct {
 	Name      string `yaml:"name"`
 	Role      string `yaml:"role"`
 	ImageLink string `yaml:"imageLink"`
 }
 
 var (
-	s              *discordgo.Session
-	appFilePath    string
-	dataFilePath   string
-	appConfig      *secretsConfig
-	heroDataConfig *heroConfig
-	initErr        error
-	mappedHeroData *map[string][]heroData
+	validRoles = []string{
+		"all", "tank", "dps", "support",
+	}
+	validImageTypes = []string{
+		"jpeg", "png", "gif",
+	}
 
 	commands = []*discordgo.ApplicationCommand{
 		{
-			Name:        "roll",
+			Name:        "hero_roll",
 			Description: "roll a random hero",
 			Options: []*discordgo.ApplicationCommandOption{
 				{
 					Type:        discordgo.ApplicationCommandOptionString,
-					Name:        "hero_role",
-					Description: "Hero roles to consider. Valid values are 'all' (default) 'dps' 'support' 'tank'",
+					Name:        "role",
+					Description: "Roles to consider. Valid values are 'all' (default) 'dps' 'support' 'tank'",
 					Required:    false,
 				},
 			},
 		},
 	}
-
-	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
-		"roll": func(_ *discordgo.Session, i *discordgo.InteractionCreate) {
-			// parse option data into a useable form
-			options := i.ApplicationCommandData().Options
-			optionMap := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(options))
-			for _, opt := range options {
-				optionMap[opt.Name] = opt
-			}
-			margs := make([]interface{}, 0, len(options))
-			msgformat := "You learned how to use command options! " +
-				"Take a look at the value(s) you entered:\n"
-
-			if option, ok := optionMap["hero_role"]; ok {
-				margs = append(margs, option.StringValue())
-				msgformat += "> hero role option %s\n"
-			}
-
-			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionResponseData{
-					Content: fmt.Sprintf(
-						msgformat,
-						margs...,
-					),
-				},
-			})
-		},
-	}
 )
 
 // establish flags and read config values
-func init() {
+func readAppFlags() (string, string) {
 	const (
-		defaultValue = ""
-		usage        = "path to config file"
+		defaultValue    = ""
+		discordOptUsage = "path to Discord config file"
+		heroOptUsage    = "path to hero data file"
 	)
-	flag.StringVar(&appFilePath, "file", defaultValue, usage)
-	flag.StringVar(&appFilePath, "f", defaultValue, usage)
-	flag.StringVar(&dataFilePath, "data-file-name", defaultValue, usage)
-	flag.StringVar(&dataFilePath, "d", defaultValue, usage)
+
+	var discordConfigPath, heroConfigPath string
+	flag.StringVar(&discordConfigPath, "file", defaultValue, discordOptUsage)
+	flag.StringVar(&discordConfigPath, "f", defaultValue, discordOptUsage)
+	flag.StringVar(&heroConfigPath, "data-file-name", defaultValue, heroOptUsage)
+	flag.StringVar(&heroConfigPath, "d", defaultValue, heroOptUsage)
 	flag.Parse()
+	return discordConfigPath, heroConfigPath
 }
 
-// read config to get file path and get config values
-// TODO: refactor this to make config parsing more generic
-func init() {
-	if appFilePath == "" {
-		log.Fatalf("Error: must provide a path to application config file")
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
-	if dataFilePath == "" {
-		log.Fatalf("Error: must provide a path to data config file")
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
-	appConfig, initErr = getAppConfigValues(appFilePath)
-	if initErr != nil {
-		log.Fatalf("Error: unable to parse application config YAML file %v", initErr)
-		flag.PrintDefaults()
-		os.Exit(2)
-	}
-	heroDataConfig, initErr = getHeroConfigValues(dataFilePath)
-	if initErr != nil {
-		log.Fatalf("Error: unable to parse data config YAML file %v", initErr)
-		flag.PrintDefaults()
-		os.Exit(2)
-	}
-}
-
-// TODO refactor this to make parsing more generic
-func getAppConfigValues(filePath string) (*secretsConfig, error) {
+func readConfigValues[T interface{}](filePath string) (*T, error) {
 	file, err := os.ReadFile(filePath)
 	if err != nil {
 		return nil, err
 	}
-	var config *secretsConfig
+	var config *T
 	err = yaml.Unmarshal(file, &config)
 	if err != nil {
 		return nil, err
@@ -136,44 +86,152 @@ func getAppConfigValues(filePath string) (*secretsConfig, error) {
 	return config, nil
 }
 
-func getHeroConfigValues(filePath string) (*heroConfig, error) {
-	file, err := os.ReadFile(filePath)
-	if err != nil {
-		return nil, err
+func mapByRole(heroData *heroConfig) *map[string][]*hero {
+	result := map[string][]*hero{
+		"all": make([]*hero, 0),
 	}
-	var config *heroConfig
-	err = yaml.Unmarshal(file, &config)
-	if err != nil {
-		return nil, err
+	for _, heroDatum := range *heroData.Heroes {
+		if !slices.Contains(validRoles, heroDatum.Role) {
+			log.Fatalf("error: found invalid role %v at hero %v", heroDatum.Role, heroDatum.Name)
+			os.Exit(1)
+		}
+		result["all"] = append(result["all"], &heroDatum)
+		currentRoleHeroes, ok := result[heroDatum.Role]
+		var newRoleValue []*hero
+		if ok {
+			newRoleValue = append(currentRoleHeroes, &heroDatum)
+		} else {
+			newRoleValue = []*hero{
+				&heroDatum,
+			}
+		}
+		result[heroDatum.Role] = newRoleValue
 	}
-	return config, nil
 
+	return &result
 }
 
 // connect to discord
-func init() {
+func initializeDiscordSession(config *discordConfig) (*discordgo.Session, error) {
 	fmt.Println("initializing discord connection")
-	s, initErr = discordgo.New("Bot " + appConfig.BotToken)
-	if initErr != nil {
-		log.Fatalf("Error: unable to create session %v", initErr)
-		os.Exit(3)
+	return discordgo.New("Bot " + config.BotToken)
+}
+
+func getOptionsFromDiscordInteraction(i *discordgo.InteractionCreate) map[string]string {
+	options := i.ApplicationCommandData().Options
+	optionMap := make(map[string]string, len(options))
+	for _, opt := range options {
+		optionMap[opt.Name] = opt.StringValue()
+	}
+	return optionMap
+}
+
+func getImageFileType(imageUrl string) string {
+	return filepath.Ext(imageUrl)
+}
+
+func toProtocol(protocol string, original string) (string, error) {
+	u, err := url.Parse(original)
+	if err != nil {
+		return "", err
+	}
+	u.Scheme = protocol
+	return u.String(), nil
+}
+
+func buildRollCommandHandler(heroesByRole *map[string][]*hero) func(*discordgo.Session, *discordgo.InteractionCreate) {
+	return func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		var responseContent string
+		// parse option data into a useable form
+		heroMap := *heroesByRole
+		optionMap := getOptionsFromDiscordInteraction(i)
+		roleValue, ok := optionMap["role"]
+		// default value
+		if !ok {
+			roleValue = "all"
+		}
+		// TODO add a thumbnail/profile to accompany the message
+		if !slices.Contains(validRoles, roleValue) {
+			// TODO return Widow if an invalid role is entered
+			responseContent = "Invalid role provided. Valid roles are all (default), tank, support, dps"
+			// TODO extract this into a function
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: responseContent,
+				},
+			})
+			return
+		}
+
+		validHeroes, ok := heroMap[roleValue]
+		// valid role provided but no hero data found
+		// this should never happen given the data, but is included for completeness
+		if !ok || len(validHeroes) == 0 {
+			responseContent = fmt.Sprintf("no heroes found for role %v", roleValue)
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: responseContent,
+				},
+			})
+			return
+		}
+		heroChoice := validHeroes[rand.Intn(len(validHeroes))]
+		responseContent = heroChoice.Name
+
+		if heroChoice.ImageLink == "" {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: responseContent,
+				},
+			})
+			return
+		}
+
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: responseContent,
+				// Embeds:  embeds,
+			},
+		})
+
 	}
 }
 
-func init() {
-	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
-			h(s, i)
-		}
-	})
-}
-
 func main() {
+	discordConfigPath, heroDataConfigPath := readAppFlags()
+	if discordConfigPath == "" || heroDataConfigPath == "" {
+		log.Fatalf("Error: missing required file path")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	appConfigValues, err := readConfigValues[discordConfig](discordConfigPath)
+	if err != nil {
+		log.Fatalf("unable to read Discord connection configuration: %v", err)
+		os.Exit(1)
+	}
+
+	heroData, err := readConfigValues[heroConfig](heroDataConfigPath)
+	if err != nil {
+		log.Fatalf("unable to read hero data: %v", err)
+		os.Exit(1)
+	}
+	mappedHeroData := mapByRole(heroData)
+	heroRollCommandHandler := buildRollCommandHandler(mappedHeroData)
+	s, err := initializeDiscordSession(appConfigValues)
+	if err != nil {
+		log.Fatalf("unable to initialize discord session: %v", err)
+	}
+
 	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
 		log.Printf("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
 	})
 	log.Println("opening session")
-	err := s.Open()
+	err = s.Open()
 	if err != nil {
 		log.Fatalln("unable to open session")
 		os.Exit(4)
@@ -184,12 +242,16 @@ func main() {
 	log.Println("adding commands")
 	registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
 	for i, v := range commands {
-		cmd, err := s.ApplicationCommandCreate(s.State.User.ID, appConfig.GuildID, v)
+		cmd, err := s.ApplicationCommandCreate(s.State.User.ID, appConfigValues.GuildID, v)
 		if err != nil {
 			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+			return
 		}
 		registeredCommands[i] = cmd
 	}
+	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		heroRollCommandHandler(s, i)
+	})
 
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt)
@@ -197,6 +259,6 @@ func main() {
 	<-stop
 
 	for _, command := range registeredCommands {
-		s.ApplicationCommandDelete(command.ApplicationID, appConfig.GuildID, command.ID)
+		s.ApplicationCommandDelete(command.ApplicationID, appConfigValues.GuildID, command.ID)
 	}
 }
