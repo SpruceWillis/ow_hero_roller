643d09dabd13c616cbb1ead775078beaecdddc12
Kevin Huang <kevinht.huang@gmail.com>
2025-11-10 09:26:47 -0800

    rename file back to be more flexible

--- build_container.sh
+++ build_container.sh
@@ -2,4 +2,5 @@
 
 TAG="us-west1-docker.pkg.dev/ow-hero-roller-1741411768301/ow-hero-roller-docker/ow_hero_roller:$(git rev-parse --short HEAD)"
 docker build -t $TAG --platform linux/amd64 .
-docker push $TAG
+echo "Built image with tag: $TAG"
+echo "Run 'docker push $TAG' to push the image to the container registry."
