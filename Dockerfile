2bff3590906b5e96077bd3659557b56ca5425273
Kevin Huang <kevinht.huang@gmail.com>
2025-11-10 09:37:44 -0800

    maybe fix build issues

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ ARG ARCH=amd64
 
 WORKDIR /usr/src/app
 COPY go.* main.go /usr/src/app/
-RUN  GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o /usr/local/bin/ow_hero_roller .
+RUN  GOOS=${OS} GOARCH=${ARCH} CGO_ENABLED=0 go build -o /usr/local/bin/ow_hero_roller .
 
 FROM scratch
 COPY --from=builder /usr/local/bin/ow_hero_roller /usr/local/bin/ow_hero_roller
--- build_container.sh
+++ build_container.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 TAG="us-west1-docker.pkg.dev/ow-hero-roller-1741411768301/ow-hero-roller-docker/ow_hero_roller:$(git rev-parse --short HEAD)"
-docker build -t $TAG --platform linux/amd64 .
+docker buildx build -t $TAG --platform linux/amd64 .
 echo "Built image with tag: $TAG"
 echo "Run 'docker push $TAG' to push the image to the container registry."
