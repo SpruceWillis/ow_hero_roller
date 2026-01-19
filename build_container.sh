072036d0a14096dc46defd3ada20d5606711c28a
Kevin Huang <kevinht.huang@gmail.com>
2026-01-19 00:34:32 -0800

    add conditional push to container script
    remove debugging line

--- build_container.sh
+++ build_container.sh
@@ -1,6 +1,19 @@
 #! /bin/bash
 
+PUSH=false
+
+while getopts "p" flag; do
+    case "${flag}" in
+        p) PUSH=true ;;
+    esac
+done
+
 TAG="us-west1-docker.pkg.dev/ow-hero-roller-1741411768301/ow-hero-roller-docker/ow_hero_roller:$(git rev-parse --short HEAD)"
 docker buildx build -t $TAG --platform linux/amd64 .
 echo "Built image with tag: $TAG"
-echo "Run 'docker push $TAG' to push the image to the container registry."
+
+if [ "$PUSH" = true ]; then
+    docker push $TAG
+else
+    echo "Run 'docker push $TAG' to push the image to the container registry."
+fi
