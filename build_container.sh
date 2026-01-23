23b9c4a9fe7407e75fcb9c0d943cc6b574c659ca
Kevin Huang <kevinht.huang@gmail.com>
2026-01-23 00:56:20 -0800

    add version flag for use with auto deploy

--- build_container.sh
+++ build_container.sh
@@ -3,16 +3,18 @@
 PUSH=false
 REGION='us-west1'
 TAG_BASE=''
+VERSION=$(git rev-parse --short HEAD)"
 
-while getopts ":pr:t:" flag; do
+while getopts ":pr:t:v:" flag; do
     case "${flag}" in
         p) PUSH=true ;;
         r) REGION=${OPTARG} ;;
         t) TAG_BASE=${OPTARG}
+        v) VERSION=${OPTARG}
     esac
 done
 
-TAG="${REGION}-docker.pkg.dev/${TAG_BASE}:$(git rev-parse --short HEAD)"
+TAG="${REGION}-docker.pkg.dev/${TAG_BASE}:${VERSION}
 docker buildx build -t $TAG --platform linux/amd64 .
 echo "Built image with tag: $TAG"
 
