7484076237a187e1b99b2f4f0c191c8d1637628e
Kevin Huang <kevinht.huang@gmail.com>
2026-01-23 01:14:07 -0800

    fix syntax problems in build script

--- build_container.sh
+++ build_container.sh
@@ -3,18 +3,18 @@
 PUSH=false
 REGION='us-west1'
 TAG_BASE=''
-VERSION=$(git rev-parse --short HEAD)"
+VERSION=$(git rev-parse --short HEAD)
 
 while getopts ":pr:t:v:" flag; do
     case "${flag}" in
         p) PUSH=true ;;
         r) REGION=${OPTARG} ;;
-        t) TAG_BASE=${OPTARG}
-        v) VERSION=${OPTARG}
+        t) TAG_BASE=${OPTARG} ;;
+        v) VERSION=${OPTARG} ;;
     esac
 done
 
-TAG="${REGION}-docker.pkg.dev/${TAG_BASE}:${VERSION}
+TAG="${REGION}-docker.pkg.dev/${TAG_BASE}:${VERSION}"
 docker buildx build -t $TAG --platform linux/amd64 .
 echo "Built image with tag: $TAG"
 
